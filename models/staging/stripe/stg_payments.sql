262944f5bb1cbbcf522ed21764a0816976c49d94
Chris Laun <chrislaun@gmail.com>
2021-01-15 20:42:24 +0000

    dbt training week 2 work

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,15 @@
+ with payments as (
+
+    select
+        orderid as order_id,
+        id as customer_id,
+        amount / 100 as amount,
+        paymentmethod as payment_method,
+        status,
+        created as created_at
+
+    from {{ source('stripe', 'payment')}}
+
+)
+
+select * from payments
